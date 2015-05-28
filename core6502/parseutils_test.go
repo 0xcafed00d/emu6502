9a301dc20447eecca3ac64cb454d8c53ba7563fd
Lee Witek <neomech@gmail.com>
2015-05-28 23:48:24 +0100

    messing with assertions

--- core6502/parseutils_test.go
+++ core6502/parseutils_test.go
@@ -5,9 +5,25 @@ import (
 	"testing"
 )
 
+type T testing.T
+
+func (t *T) AssertEqualsNoErr(val interface{}, err error) func(val interface{}) {
+	return func(val interface{}) {
+
+	}
+}
+
+func (t *T) AssertErr(val interface{}, err error) {
+	testbuddy.AssertEqual((*testing.T)(t), val, nil)
+}
+
 func TestSplit(t *testing.T) {
+	tt := (*T)(t)
+
 	testbuddy.AssertEqual(t, Split("", " \t"), []string{})
 	testbuddy.AssertEqual(t, Split("   \t\t\t", " \t"), []string{})
 	testbuddy.AssertEqual(t, Split("hello world", " \t"), []string{"hello", "world"})
 	testbuddy.AssertEqual(t, Split("    hello     world     ", " \t"), []string{"hello", "world"})
+
+	tt.AssertEqualsNoErr(ParseUint("123", 16))(1)
 }
