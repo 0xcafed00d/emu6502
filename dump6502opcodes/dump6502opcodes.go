b1c30024ccbd512d8168c6b421c65e19703645d1
Lee Witek <neomech@gmail.com>
2015-12-31 01:19:50 +0000

    print full hex opcode

--- dump6502opcodes/dump6502opcodes.go
+++ dump6502opcodes/dump6502opcodes.go
@@ -8,11 +8,18 @@ import (
 func main() {
 	var ctx core6502.BasicCPUContext
 	core6502.HardResetCPU(&ctx, 0x400)
+	ctx.PokeWord(0x401, 0xeeff)
 
 	for opcode := 0; opcode < 256; opcode++ {
 		ctx.Poke(0x400, uint8(opcode))
 
-		dis, _ := core6502.Disassemble(&ctx, 0x400)
-		fmt.Printf("$%02x %s\n", opcode, dis)
+		dis, len := core6502.Disassemble(&ctx, 0x400)
+		if len == 1 {
+			fmt.Printf("$%02x:              %s\n", opcode, dis)
+		} else if len == 2 {
+			fmt.Printf("$%02x $%02x:          %s\n", opcode, ctx.Peek(0x401), dis)
+		} else if len == 3 {
+			fmt.Printf("$%02x $%02x $%02x:      %s\n", opcode, ctx.Peek(0x401), ctx.Peek(0x402), dis)
+		}
 	}
 }
